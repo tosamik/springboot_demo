732cff630fc101f3c4e778d6e29886c19ad45bd4
tosamik <tosamik@gmail.com>
2017-06-18 00:04:31 +0800

    my first Thymeleaf demo

--- learning-springboot/src/main/java/com/samik/learning/springboot/learningspringboot/SpringBootControllerDemo.java
+++ learning-springboot/src/main/java/com/samik/learning/springboot/learningspringboot/SpringBootControllerDemo.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringBootControllerDemo {
 
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String hello() {
         return "Hello";
     }
--- learning-springboot/src/test/java/com/samik/learning/springboot/learningspringboot/SpringbootApplicationDemoTests.java
+++ learning-springboot/src/test/java/com/samik/learning/springboot/learningspringboot/SpringbootApplicationDemoTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class LearningSpringbootApplicationTests {
+public class SpringbootApplicationDemoTests {
 
 	@Test
 	public void contextLoads() {
